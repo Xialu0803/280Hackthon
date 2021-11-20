90e5d3503ecc886a516c6b311cf0b08306ffbc71
Xialu0803 <xialu.zou@sjsu.edu>
2021-11-20 15:56:59 -0800

    add scale bar for year

---
+++ script2.js
@@ -0,0 +1,13 @@
+$(function () {
+    $("#slider-range").slider({
+        range: true,
+        min: 1990,
+        max: 2020,
+        values: [1990, 2020],
+        slide: function (event, ui) {
+            $("#amount").val(+ ui.values[0] + " - " + ui.values[1]);
+        }
+    });
+    $("#amount").val(+ $("#slider-range").slider("values", 0) +
+        " - " + $("#slider-range").slider("values", 1));
+});
