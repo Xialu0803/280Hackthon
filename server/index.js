a7abac81356d7675b0f7e7f4749aee2dbb23738e
Jingjing Bu <44009042+liaoliao6@users.noreply.github.com>
2021-11-20 20:24:47 -0800

    mysql
    add mysql

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+// Create connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '836353',
+    database: '280hackathon'
+})
+
+//connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+
+});
+
+//getGDP
+app.get('/getgdps', (req, res) => {
+    let sql = 'SELECT * FROM gdp';
+    let query = db.query(sql, (err, results) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(JSON.stringify(results));
+        }
+    })
+}
+
+)
+app.listen('3000', () => {
+    console.log('Sever started on port 3000');
+});
